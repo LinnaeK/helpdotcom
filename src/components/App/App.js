a06fa0c716aa5a5350845fbc9dda0008569889ea
linnaek <linnaekraemer@gmail.com>
2020-03-04 23:30:20 -0600

    create columns need to modify color

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,38 @@
 import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Column from '../Column/Column'
 import './App.css';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    '& > *': {
+      margin: theme.spacing(2),
+    },
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+  const [errorMsg, setErrorMsg]=useState([])
+  const [warningMsg, setWarningMsg]=useState([])
+  const [infoMsg, setInfoMsg]=useState([])
+
   return (
-    <div>
-      Framework Ready
+    <div className={classes.root}>
+      <Column 
+      errorMsg={errorMsg}
+      color = '#F56236'
+      />
+      <Column 
+      warningMsg={warningMsg}
+      color='#FCE788'
+      />
+      <Column 
+      infoMsg={infoMsg}
+      color=''
+      />
     </div>
   );
 }
---
+++ src/components/Title/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div>
+      <h3>Error Type props.typeNum</h3>
+      <p>Count props.columnCount</p>
+    </div>
+  );
+};
+
+export default Title;
